5926bac8126d6a98585cedea9eb3dd8ecd6451fb
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2023-10-22 10:17:35 -0700

    log player speed to the chat, yoink some code from https://github.com/zaze06/Speedometer/blob/965a3ea62df6ec2f1923c088c2728f39c9c6178d/common/src/main/java/me/zacharias/speedometer/Client.java#L214

--- src/main/kotlin/drakefruit/accelerationsfx/AccelerationSFX.kt
+++ src/main/kotlin/drakefruit/accelerationsfx/AccelerationSFX.kt
@@ -1,14 +1,29 @@
 package drakefruit.accelerationsfx
 
 import net.fabricmc.api.ModInitializer
-import org.slf4j.LoggerFactory
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.minecraft.client.MinecraftClient
+import net.minecraft.text.Text
 
 object AccelerationSFX : ModInitializer {
-	private var MOD_ID: String = "accelerationsfx"
-    private val logger = LoggerFactory.getLogger(MOD_ID)
-
 	override fun onInitialize() {
-		logger.info("Hello Fabric world!")
-	}
+		ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client ->
+			val player = client.player
+			if (player != null) {
+				val velocity = player.velocity
 
+				// Get the current tick time
+				val tickTime = MinecraftClient.getInstance().server?.tickTime ?: 50.0f
+
+				val yOffset = 0.0784000015258789
+				val xOffset = 0.0
+				val zOffset = 0.0
+				val vOffset = 0.0
+
+				val speed = (Math.sqrt(Math.pow(velocity.x + xOffset, 2.0) + Math.pow(velocity.y + yOffset, 2.0) + Math.pow(velocity.z + zOffset, 2.0)) * 20.0) + vOffset
+
+				client.inGameHud.chatHud.addMessage(Text.of("Player Speed: $speed blocks/second"))
+			}
+		})
+	}
 }
